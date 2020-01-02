611286b87e64db5f4a140b052a4874dfb1cb9aa6
sujl950620 <tlaf2000@naver.com>
2020-01-02 19:29:38 +0900

    add

--- SampleSpring/src/main/java/com/gd/test/common/controller/CommonAOP.java
+++ SampleSpring/src/main/java/com/gd/test/common/controller/CommonAOP.java
@@ -30,7 +30,8 @@ public class CommonAOP {
 			+ "&&!execution(* com.gd.test..TestController2.*Log*(..))"
 			+ "&&!execution(* com.gd.test..TestController2.*Join*(..))"
 			+ "&&!execution(* com.gd.test..TestController2.*List(..))"
-			+ "&&!execution(* com.gd.test..TestController2.*Detail(..))")
+			+ "&&!execution(* com.gd.test..TestController2.*Detail(..))"
+			)
 	public void aLoginCheckAOP() {}
 	
 	//ProceedingJoinPoint -> 대상 적용 이벤트 필터
@@ -62,5 +63,47 @@ public class CommonAOP {
 		
 		return mav;
 	}
+	
+	
+	@Pointcut("execution(* com.gd.test..BlogController.*(..))" 
+			+ "&&!execution(* com.gd.test..BlogController.*Ajax(..))"
+			+ "&&!execution(* com.gd.test..BlogController.*Log*(..))"
+			+ "&&!execution(* com.gd.test..BlogController.*Join*(..))"
+			+ "&&!execution(* com.gd.test..BlogController.*List(..))"
+			+ "&&!execution(* com.gd.test..BlogController.*Main(..))"
+			+ "&&!execution(* com.gd.test..BlogController.*Detail(..))"
+			)
+	public void bLoginCheckAOP() {}
+	
+	//ProceedingJoinPoint -> 대상 적용 이벤트 필터
+	/*
+	 * @Befor -> 메소드 실행 전
+	 * @After -> 메소드 실행 후
+	 * @After-returning -> 메소드 정상 실행 후
+	 * @After-throwing -> 메소드 예외 발생 후
+	 * @Around -> 모든 동작 시점 (어느 곳에서 세션이 날라갈지 모르니 대부분 이걸사용)
+	 */
+	//관점 : 프로그램의 실행단계, 흐름상 중간에 이벤트 끼우기 
+	@Around("bLoginCheckAOP()") //이필터에 해당하는 모든 동작 시점(위에있는 메서드를 가져와서 사용)
+	public ModelAndView bLoginCheckAOP(ProceedingJoinPoint joinPoint) throws Throwable {
+		// ProceedingJoinPoint : 진행중인 관점 시점의 이벤트,이벤트를 그대로 돌려줌
+		ModelAndView mav = new ModelAndView();
+		
+		//Request 객체 취득
+		HttpServletRequest request 
+		= ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+		
+		//Session 취득
+		HttpSession session = request.getSession();
+		if(session.getAttribute("sBmNo")!= null) {
+			mav = (ModelAndView) joinPoint.proceed(); //기존 이벤트 처리 행위를 이어서 진행
+		} else {
+			mav.setViewName("redirect:blog_Login");
+		}
+		System.out.println("-------------bLoginCheckAOP 실행됨 ---------");
+		
+		return mav;
+	}
+	
 	
 }
